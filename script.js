d69db0582ccf4204daf83adb015bc8152cfaef09
whosaldo <calefa7@gmail.com>
2023-08-16 15:36:36 +0300

    This is the final version of Homepage (an problem from cs50)
    This website is not responsive so the optimal resolution is 1920x1080.
    Other resolutions are not optimized.

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function prog()
+{
+
+ infoh.innerHTML = "Programming";
+ infoa.innerHTML = "Baffled by technology's magic, I embarked on a quest to unravel its secrets. With a keyboard as my compass and code as my guide, I ventured into the world of programming. Frustration met fascination as I tamed languages and crafted logic. Thus, my odyssey into the realm of code began.";
+ gif2.style = "display: none";
+ gif3.style = "display: block";
+ gif4.style = "display: none";
+ gif5.style = "display: none";
+}
+
+function bass()
+{
+    infoh.innerHTML = "Bass Guitar";
+    infoa.innerHTML = "With a bass in hand, curiosity ignited. Fumbling fingers found frets and strings, crafting tentative rhythms. Each note pulsed determination, every melody a step toward mastery. Hours melded into melodies; practice turned into passion. From novice to groover, my journey as a bassist began, anchoring the band's heartbeat.";
+    gif2.style = "display: none";
+    gif3.style = "display: none";
+    gif4.style = "display: block";
+    gif5.style = "display: none";
+}
+
+
+function coffee()
+{
+    infoh.innerHTML = "Coffee";
+    infoa.innerHTML = "One morning, the rich aroma of brewing coffee mingled with the melodious fingering of a bass guitar. Curiosity led me to a quaint café, where a skilled bassist played soulful tunes. Captivated, I sipped my first coffee, its warmth echoing the music's embrace. From that day, coffee and bass guitar became my harmonious ritual.";
+    gif2.style = "display: none";
+    gif3.style = "display: none";
+    gif4.style = "display: none";
+    gif5.style = "display: block";
+}
+
+
+
